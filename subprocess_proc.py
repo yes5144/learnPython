aa3760ac37539a11ac1818d0d7248ff8de7b1273
channel <1006793841@qq.com>
2018-03-22 17:07:50 +0800

    os.popen, os.system

--- subprocess_proc.py
+++ subprocess_proc.py
@@ -3,5 +3,23 @@
 import os,sys
 import subprocess
 
+# 获取执行状态返回值
+os.system('netstat -tnlp | grep http')
+
+if (os.system('netstat -tnlp |grep http') == 0):
+    print('process http exists')
+
+# 获取执行结果os.popen，
+# 更为强大的是subprocess.Popens
+if (os.popen('netstat -ntlp |grep http').read() !=''):
+    print('process http exists')
+
 print(os.system('dir'))
 print(os.system('ls'))
+
+subprocess.getstatusoutput('ls /bin/ls')
+
+subprocess.getstatusoutput('cat /bin/junk')
+
+subprocess.getstatusoutput('/bin/junk')
+
