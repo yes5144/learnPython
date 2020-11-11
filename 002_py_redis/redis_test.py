33656d760497cd023fb38fc23e9fbb0190f9157b
channel <1006793841@qq.com>
2020-11-11 18:41:16 +0800

    redis

--- 002_py_redis/redis_test.py
+++ 002_py_redis/redis_test.py
@@ -6,3 +6,8 @@ r = redis.Redis(host='192.168.1.174',
                 password='3cgM0v2P6csVNxxxxxxxacWIf2i70eO')
 r.set('hgg_session', '2020-09-04')
 print(r.get('hgg_session'))
+loglen = r.slowlog_len()
+print(r.slowlog_get(loglen))
+
+# cli save rdb to local
+# bin/redis-cli -h 192.168.0.2 -p 6379 -a 'password'  --rdb xxx.rdb
