1e763da2b6eab612e921996c6f6e841e3b7d79dc
yes5144 <1006793841@qq.com>
2020-12-05 15:42:34 +0800

    str format

--- 000_py_base/str_test.py
+++ 000_py_base/str_test.py
@@ -1,3 +1,10 @@
-str = '\xbe\xdc\xbe\xf8\xb7\xc3\xce\xca\xa1\xa3'
-b = repr(str)
-print(unicode(eval(b), "gbk"))
+# str = '\xbe\xdc\xbe\xf8\xb7\xc3\xce\xca\xa1\xa3'
+# b = repr(str)
+# print(unicode(eval(b), "gbk"))
+
+input_name = "sz; drop databases demo"
+params = [input_name]
+print(params)
+
+sql = 'select * from goods where name="%s"' % params
+print(sql)
